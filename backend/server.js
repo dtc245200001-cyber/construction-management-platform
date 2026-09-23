f6cde4cf4c040dc79428d9ac040101e3489fbf24
Bui Van Dat <Dtc245090046@ictu.edu.vn>
2026-09-23 15:46:51 +0700

    fix sever.js

--- backend/server.js
+++ backend/server.js
@@ -5,67 +5,39 @@ require("dotenv").config();
 
 const pool = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-
 const categoryRoutes = require("./routes/categoryRoutes");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Cho phép frontend localhost:5173 gọi backend localhost:3000
-app.use(
-  cors({
+app.use(cors({
     origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+    credentials: true
+}));
 
-// Đọc dữ liệu từ request
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Session
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "dev-secret-key",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      maxAge: 60 * 60 * 1000,
-    },
-  })
+    session({
+        secret: process.env.SESSION_SECRET || "dev-secret-key",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false,
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000
+        }
+    })
 );
 
-// Route đăng nhập
 app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoryRoutes);
 
-app.use("/api", categoryRoutes);
-// Kiểm tra backend
 app.get("/", (req, res) => {
-  res.json({
-    message: "Backend is running!",
-  });
+    res.send("API is running...");
 });
 
-// Kiểm tra PostgreSQL
-app.get("/db-test", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-
-    res.json({
-      message: "PostgreSQL connected!",
-      time: result.rows[0].now,
-    });
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).json({
-      message: "Database connection failed",
-      error: error.message,
-    });
-  }
-});
-
-// Khởi động server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
